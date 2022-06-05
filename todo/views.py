9cbf73e69721affdc145d35d55e1bdd281136a3b
igamigam <m.igarashi0307@gmail.com>
2022-06-05 22:57:47 +0900

    【add】CreateViewを使用したデータの登録機能を実装

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 from .models import TodoModel
 
 # Create your views here.
@@ -11,3 +12,10 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
     template_name = 'detail.html'
     model = TodoModel
+
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = TodoModel
+    fields = ('title', 'memo', 'priority', 'duedate')
+    success_url = reverse_lazy('list')
+
