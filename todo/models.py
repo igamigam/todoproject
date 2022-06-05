8b18b89105caa103b9b8c3240f4bc651d0dd6702
igamigam <m.igarashi0307@gmail.com>
2022-06-05 17:55:09 +0900

    【add】ListViewを使用したデータの表示機能を実装

--- todo/models.py
+++ todo/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 
 class TodoModel(models.Model):
-    title = models.CharField(max_length=100)
+    titlle = models.CharField(max_length=100)
     memo = models.TextField(max_length=255)
+    def __str__(self):
+        return self.titlle
